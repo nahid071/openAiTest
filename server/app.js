820043bc2fe9a126fbecc6bdc91f0d48e172e987
nahid071 <mdnahid071@gmail.com>
2023-01-03 17:29:33 +0545

    client removed & server re-configured

--- server/app.js
+++ server/app.js
@@ -19,18 +19,8 @@ app.use(express.urlencoded({ extended: false }));
 app.use("/api/users", require("./routes/userRoutes"));
 app.use("/api/notes", require("./routes/noteRoutes"));
 
-// serve frontend
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../frontend/build")));
+app.get("/", (req, res) => res.send("Everything's under control BOSS!"));
 
-  app.get("*", (req, res) =>
-    res.sendFile(
-      path.resolve(__dirname, "../", "frontend", "build", "index.html")
-    )
-  );
-} else {
-  app.get("/", (req, res) => res.send("Please set to production"));
-}
 
 app.use(errorHandler);
 
